a96f68967d528a944e037b3dd15fac229c824f10
Idoma <ido.masubi@gmail.com>
2021-02-25 20:45:50 +0200

    Updated street

--- street.py
+++ street.py
@@ -1,4 +1,13 @@
+from intersection import Intersection
+
 class Street:
-    def __init__(self, name, duration):
+    #                                                   ingoing     outgoing
+    def __init__(self, name, duration, intersections: (Intersection, Intersection)):
         self.name = name
         self.duartion = duration
+
+        self.ingoing_intersection = intersections[0]
+        self.outgoing_intersection = intersections[1]
+
+        self.ingoing_intersection.add_ingoing_street(self.name)
+        self.outgoing_intersection.add_outgoing_street(self.name)
