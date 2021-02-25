96f490c515fda2d6a6b5c2e887d8357a7d685aa9
Aviv Pozeilov <avivpaz10@gmail.com>
2021-02-25 22:18:49 +0200

    scheduler stupid function, 2 sec per street

--- scheduler.py
+++ scheduler.py
@@ -2,16 +2,12 @@
 from typing import List
 from street import Street
 
-class scheduler:
+class Scheduler:
 	# this clase is used for every intersection.
 	# list of incoming start + time
 
 	def __init__(self, ingoing_streets : List[Street]):
 		self.ingoing_streets = ingoing_streets
 
-	def add_street(street, duration):
-		for street_name, duration in self.scheduler_list:
-			if street == street_name:
-				assert False, "scheduler can't contain two streets!"
-
-		self.scheduler_list.append((street, duration))			
+	def very_stupid_schedule():
+		return [(street.name, 2) for street in self.ingoing_streets]
