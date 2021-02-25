2e3fc30b10a48dc2062a3dc9063a6dc30b4b7092
Idoma <ido.masubi@gmail.com>
2021-02-25 20:55:46 +0200

    Changed API after aviv complained

--- street.py
+++ street.py
@@ -1,13 +1,12 @@
 from intersection import Intersection
 
 class Street:
-    #                                                   ingoing     outgoing
-    def __init__(self, name: str, length: int, intersections: (Intersection, Intersection)):
+    def __init__(self, intersection1, intersection2, name: str, length: int):
         self.name = name
         self.length = length
 
-        self.ingoing_intersection = intersections[0]
-        self.outgoing_intersection = intersections[1]
+        self.ingoing_intersection = intersection1
+        self.outgoing_intersection = intersection2
 
         self.ingoing_intersection.add_ingoing_street(self.name)
         self.outgoing_intersection.add_outgoing_street(self.name)
@@ -15,8 +14,11 @@ class Street:
 if __name__ == '__main__':
     a = Intersection(1)
     b = Intersection(2)
+    c = Intersection(3)
 
-    street = Street("Bruh", 3, (a, b))
+    street = Street(a, b, 'Bruh', 3)
+    street2 = Street(b, c, "Eli eildis blvd.", 6)
     print('Street ->', street.__dict__)
+    print('Street ->', street2.__dict__)
     print('Intersection ->', a.__dict__)
     print('Intersection ->', b.__dict__)
