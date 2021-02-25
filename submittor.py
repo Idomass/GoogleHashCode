0f8a2d082f0195df8d16926947ddcd0e200d1f50
Omersh <omershafrir6@gmail.com>
2021-02-25 22:33:30 +0200

    more submit

--- submittor.py
+++ submittor.py
@@ -1,13 +1,15 @@
 from scheduler import Scheduler
 from typing import Dict
 
-# scheds_list = {inter1_id:(ingoing_street_name1, green_time), inter2_id:(ingoing_street_name2, green_time)}...
+# scheds_list = {inter1_id:[(ingoing_street_name1, green_time), (), ()..], inter2_id:(ingoing_street_name2, green_time)}...
 
 def create_submittion(scheds_list: Dict[int, tuple]):
     with open("solution.txt", 'x') as f:
         f.write(len(sched_list), "\n")
-        for sched in scheds:
-            f.write(len(sched))
+        for intersection, sched in scheds,items():
+            f.write(intersection)
+            f.write(len(sched), "\n")
+            f.write()
 
 
 
