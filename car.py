368de0f50c6a71e087e70e5769ecd4b4d32b1d1b
Omersh <omershafrir6@gmail.com>
2021-02-25 20:52:30 +0200

    initial car

---
+++ car.py
@@ -0,0 +1,10 @@
+from typing import List
+from steet import Street
+
+class Car():
+    def __init__(self, start_street: str, path_len: int, path: List[str]):
+        self.start_street = start_street
+        self.path_len = path_len
+        self. path = path
+
+
