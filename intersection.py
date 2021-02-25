7601e8b01a8476de39cdd74967e4489e383197dd
amitg <amitg@gmail.com>
2021-02-25 22:08:07 +0200

    intersection now have scheduler

--- intersection.py
+++ intersection.py
@@ -7,6 +7,9 @@ class Intersection:
         self.ingoing_streets = []
         self.outgoing_streets = []
         queue_in_street = {}
+        self.scheduler = {}
+
+
 
     def add_ingoing_street(self, street_name: Street):
         self.ingoing_streets.append(street_name)
@@ -22,6 +25,10 @@ class Intersection:
                 queue_in_street[car.path[car.current_street_index].street_name].append(car)
             else:
                 queue_in_street[car.path[car.current_street_index].street_name] = [car]
+
+    def calculate_scheduler():
+    	self.scheduler.add_ingoing_streets(self.ingoing_streets)
+    	self.scheduler.calculate()
 
     def __str__(self):
         out_str = '--------------------------\n'
