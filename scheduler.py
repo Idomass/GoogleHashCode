c05df824939a5c1329dc2581b82d92b167cb205e
amitg <amitg@gmail.com>
2021-02-25 22:42:33 +0200

    add solution for scheduler

--- scheduler.py
+++ scheduler.py
@@ -11,3 +11,19 @@ class Scheduler:
 
 	def very_stupid_schedule():
 		return [(street.name, 2) for street in self.ingoing_streets]
+
+	def calculate_according_to_total_cars(self):
+		total_cars_in_scheduler = 0
+		average_seconds_for_street = 5
+		my_scheduler = []
+		for street in self.ingoing_streets:
+			total_cars_in_scheduler += street.total_cars_number
+
+		for street in self.ingoing_streets:
+			street_green_time = int((street.total_cars_number / total_cars_in_scheduler) * average_seconds_for_street * len(self.ingoing_streets))
+			my_scheduler.append((street.name, street_green_time))
+
+		return my_scheduler
+
+
+
