af7c5a18a1b6f71857bf4ecad2d6497d63694aa4
Aviv Pozeilov <avivpaz10@gmail.com>
2021-02-25 22:00:12 +0200

    added car to queue

--- intersection.py
+++ intersection.py
@@ -1,15 +1,27 @@
+from street import Street
+
 class Intersection:
     def __init__(self, id):
         self.id = id
 
         self.ingoing_streets = []
         self.outgoing_streets = []
+        queue_in_street = {}
 
-    def add_ingoing_street(self, street_name: str):
+    def add_ingoing_street(self, street_name: Street):
         self.ingoing_streets.append(street_name)
 
-    def add_outgoing_street(self, street_name: str):
+    def add_outgoing_street(self, street_name: Street):
         self.outgoing_streets.append(street_name)
+
+    def add_car_to_street_queue(self, car: Car):
+        if car.is_driving:
+            return
+        else:
+            if car.path[car.current_street_index].street_name in queue_in_street:
+                queue_in_street[car.path[car.current_street_index].street_name].append(car)
+            else:
+                queue_in_street[car.path[car.current_street_index].street_name] = [car]
 
     def __str__(self):
         out_str = '--------------------------\n'
