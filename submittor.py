7861ae1ad7d041ce8aa5b25cbb960f6871d1f050
Aviv Pozeilov <avivpaz10@gmail.com>
2021-02-25 23:14:43 +0200

    sadsf

--- submittor.py
+++ submittor.py
@@ -5,8 +5,14 @@ from typing import Dict, List
 
 def create_submittion(solution: Dict[int, List[tuple]], file_name):
     with open("{name}_solution".format(name=file_name), 'w') as f:
-        f.write(str(len(solution)) + '\n')
+        counter = 0
         for intersection, sched in solution.items():
+            if len(sched) != 0:
+                counter +=1
+        f.write(str(counter) + '\n')
+        for intersection, sched in solution.items():
+            if len(sched) == 0:
+                continue
             f.write(str(intersection) + '\n')
             f.write(str(len(sched)) + '\n')
             for street in sched:
