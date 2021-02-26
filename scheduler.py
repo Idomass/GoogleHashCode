66e701b7899811313e9cfeedb825fc4e3a1c23e0
amitg <amitg@gmail.com>
2021-02-26 14:11:57 +0200

    fix scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -14,7 +14,7 @@ class Scheduler:
 
     def calculate_according_to_total_cars(self):
         total_cars_in_scheduler = 0
-        average_seconds_for_street = 30
+        average_seconds_for_street = 3
         my_scheduler = []
         for street in self.ingoing_streets:
             total_cars_in_scheduler += street.total_cars_number
@@ -23,9 +23,12 @@ class Scheduler:
             street_green_time = 0
             if total_cars_in_scheduler != 0:
                 street_green_time = int((street.total_cars_number / total_cars_in_scheduler) * average_seconds_for_street * len(self.ingoing_streets))
-
             if street_green_time != 0:
                 my_scheduler.append((street.name, street_green_time))
+            else:
+                if street.total_cars_number != 0:
+                    my_scheduler.append((street.name, 1))
+
 
         return my_scheduler
 
